06a07d6338e15b5e1c17807c9ad89efa88ba134c
oseasmoreto <oseas_moreto@hotmail.com>
2022-12-27 12:10:01 -0300

    Adiciona informações de perfil vindas da API do github

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -5,17 +5,53 @@ import {
   faUserGroup,
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useCallback, useEffect, useState } from 'react'
+import { api } from '../../lib/api'
 import { ProfileContainer } from './styles'
 
+interface User {
+  name: string
+  user: string
+  bio: string
+  company: string
+  photo: string
+  followers: number
+  url: string
+}
+
 export function Profile() {
+  const [user, setUser] = useState<User | null>(null)
+
+  const getUser = useCallback(async (query?: string) => {
+    const { data } = await api.get(`/users/${import.meta.env.VITE_GITHUB_USER}`)
+
+    const userData: User = {
+      name: data.name,
+      user: data.login,
+      company: data.company,
+      bio: data.bio,
+      photo: data.avatar_url,
+      followers: data.followers,
+      url: data.html_url,
+    }
+
+    setUser(userData)
+  }, [])
+
+  useEffect(() => {
+    getUser()
+  }, [getUser])
+
+  if (user === null) return <></>
+
   return (
     <ProfileContainer>
-      <img src="https://github.com/oseasmoreto.png" alt="Oséas Moreto" />
+      <img src={user.photo} alt={user.name} />
       <div className="box-profile">
         <div className="header">
-          <h3>Oséas Moreto</h3>
+          <h3>{user.name}</h3>
           <a
-            href="https://github.com/oseasmoreto"
+            href={user.url}
             target="_blank"
             rel="noreferrer"
             className="link-github"
@@ -24,20 +60,17 @@ export function Profile() {
             <FontAwesomeIcon fontSize={12} icon={faArrowUpRightFromSquare} />
           </a>
         </div>
-        <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        <p>{user.bio}</p>
         <ul>
           <li>
-            <FontAwesomeIcon fontSize={18} icon={faGithub} /> oseasmoreto
+            <FontAwesomeIcon fontSize={18} icon={faGithub} /> {user.user}
           </li>
           <li>
-            <FontAwesomeIcon fontSize={18} icon={faBuilding} /> web.art group
+            <FontAwesomeIcon fontSize={18} icon={faBuilding} /> {user.company}
           </li>
           <li>
-            <FontAwesomeIcon fontSize={18} icon={faUserGroup} /> 10 seguidores
+            <FontAwesomeIcon fontSize={18} icon={faUserGroup} />{' '}
+            {user.followers} seguidores
           </li>
         </ul>
       </div>
